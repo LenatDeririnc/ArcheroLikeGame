243907ae967480e542c655ef51ad1f598911040d
Alexey Kavin <smillerlenatderirinc@gmail.com>
2021-10-11 13:58:35 +0300

    Дроп нескольких монет

--- Assets/Scripts/Components/Money.cs
+++ Assets/Scripts/Components/Money.cs
@@ -7,17 +7,22 @@ namespace Components
     public class Money : MonoBehaviour
     {
         [SerializeField] private int cost;
-        
+
+        private Rigidbody m_rigidbody;
+        public Rigidbody Rigidbody => m_rigidbody;
         private HUDController m_hudController;
         private GarbageCollector m_garbageCollector;
         
         private Transform m_transform;
+        public Transform Transform => m_transform;
         private GameObject m_gameObject;
+        public GameObject GameObject => m_gameObject;
 
         private void Awake()
         {
             m_transform = transform;
             m_gameObject = gameObject;
+            m_rigidbody = GetComponent<Rigidbody>();
             
             m_hudController = HUDController.self;
             HUDController.ONHUDControllerInit += () => m_hudController = HUDController.self;
---
+++ Assets/Scripts/Components/MoneyDrop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Components
+{
+    public class MoneyDrop : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_moneyPrefab;
+        [SerializeField] private int dropCount;
+        [SerializeField] private float forceDrop;
+        private Transform m_transform;
+
+        private void Awake()
+        {
+            m_transform = transform;
+        }
+
+        private void Start()
+        {
+            for (int i = 0; i < dropCount; ++i)
+            {
+                var randX = Random.value - 0.5f;
+                var randY = Random.value - 0.5f;
+                
+                Vector3 randomDirection = new Vector3(randX, 0, randY).normalized;
+                var newMoney = Instantiate(m_moneyPrefab);
+
+                Money newMoneyClass = newMoney.GetComponent<Money>();
+                newMoneyClass.Transform.position = m_transform.position;
+                newMoneyClass.Rigidbody.AddForce(randomDirection * forceDrop, ForceMode.VelocityChange);
+            }
+        }
+    }
+}
