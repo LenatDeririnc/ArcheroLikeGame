9741057a967138cd675977d1828c3e241f1e2e5c
Alexey Kavin <smillerlenatderirinc@gmail.com>
2021-10-11 14:23:58 +0300

    Автоматический подбор монеток

---
+++ Assets/Scripts/Components/Characters/Player/PlayerPointer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Components.Characters.Player
+{
+    public class PlayerPointer : MonoBehaviour
+    {
+        public CharacterProperties m_properties;
+        public static PlayerPointer self;
+        public static Action ONPlayerPointerInit; 
+
+        private void Awake()
+        {
+            self = this;
+            
+            m_properties = GetComponent<CharacterProperties>();
+            CharacterProperties.ONCharacterPropertiesInit += () => m_properties = GetComponent<CharacterProperties>();
+            
+            ONPlayerPointerInit?.Invoke();
+        }
+    }
+}
--- Assets/Scripts/Components/Money.cs
+++ Assets/Scripts/Components/Money.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections;
+using Components.Characters.Player;
 using Components.HUD;
 using UnityEngine;
+using Tools.Timer;
 
 namespace Components
 {
     public class Money : MonoBehaviour
     {
         [SerializeField] private int cost;
+        [SerializeField] private float collectSpeed = 1f;
 
         private Rigidbody m_rigidbody;
         public Rigidbody Rigidbody => m_rigidbody;
@@ -17,6 +21,11 @@ namespace Components
         public Transform Transform => m_transform;
         private GameObject m_gameObject;
         public GameObject GameObject => m_gameObject;
+
+        private Timer m_timer;
+
+        private PlayerPointer m_playerPointer;
+        private bool m_isGoingToPlayer = false;
 
         private void Awake()
         {
@@ -29,17 +38,40 @@ namespace Components
             
             m_garbageCollector = GarbageCollector.self;
             GarbageCollector.GarbageCollectorInited += () => m_garbageCollector = GarbageCollector.self;
+            
+            m_playerPointer = PlayerPointer.self;
+            PlayerPointer.ONPlayerPointerInit += () => m_playerPointer = PlayerPointer.self; 
         }
 
         private void Start()
         {
             m_transform.parent = m_garbageCollector.Transform;
+            m_timer = new Timer(this);
+            m_timer.Start(DelayForMove());
         }
 
         public void PickUp()
         {
             m_hudController.AddMoney(cost);
             m_gameObject.SetActive(false);
+        }
+
+        IEnumerator DelayForMove()
+        {
+            yield return new WaitForSeconds(1f);
+            m_rigidbody.isKinematic = true;
+            m_isGoingToPlayer = true;
+        }
+
+        private void Update()
+        {
+            if (!m_isGoingToPlayer)
+                return;
+
+            var PlayerTransform = m_playerPointer.m_properties.Transform;
+            var direction = (PlayerTransform.position - m_transform.position).normalized;
+
+            m_transform.position += direction * collectSpeed * Time.deltaTime;
         }
     }
 }
